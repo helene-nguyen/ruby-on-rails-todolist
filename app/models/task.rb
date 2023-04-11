f170ea3867955fd8a863e72f88bf73f030a1aad6
Yumicode <75518128+helene-nguyen@users.noreply.github.com>
2023-04-11 07:18:15 +0200

    change task test sass preprocessor

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,13 @@ class Task < ApplicationRecord
   #          "not yet": false,
   #          "done": true,
   #        }
+
+  # display only tasks that don't have deleted_at, it keeps into db, applied everywhere, priority above controller
+  default_scope { where("deleted_at IS NULL") }
+  # scope :not_deleted, -> { where("deleted_at IS NULL") }
+
+  # scope :deleted, -> { where("deleted_at IS NOT NULL") }
+  # in controller     @tasks = Task.deleted
+  # if there is a scope but we don't want the scope
+  # in controller @tasks = Task.unscope.deleted
 end
